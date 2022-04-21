8b24910cb44b5cf64252c9311135af72c6fc0fe9
Arun Vijayshankar <arunvijayshankar@gmail.com>
2022-04-20 22:27:40 -0700

    moved to src/

---
+++ src/grepicpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "grepicpplib.h"
+
+using namespace std;
+
+
+int main(int argc, // argument count
+        char *argv[], // pointer to an array of strings of passed arguments
+        char *envp[] // pointer to an array of strings of env variables
+        )
+{
+
+    CONFIG nConfig(argc, argv);
+
+    cout << "Searching for `" << nConfig.query << "`\n";
+    cout << "In file `" << nConfig.filename << "`\n";
+
+    run(nConfig);
+}
